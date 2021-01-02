5e7e93e3e088c9b93ec830e365521c40fd76d87f
kanisan007 <sabu01.kani@gmail.com>
2021-01-02 12:59:21 +0900

    pienver

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,20 @@
+import os
+import ssl
+
+from os.path import join, dirname
+
 from discord.ext import commands
 import os
 import traceback
+
+from dotenv import load_dotenv
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -15,7 +29,7 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send('pien')
 
 
 bot.run(token)
